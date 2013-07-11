270a151d2fd17f3719a5b7349d16cf3a4e06fcef
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2013-07-11 21:24:52 +0200

    Fix bug in fish_prompt

--- common/fish-functions/fish_prompt.fish
+++ common/fish-functions/fish_prompt.fish
@@ -2,7 +2,6 @@ function git_prompt
 	if git rev-parse 2> /dev/null
 		set branch (git status -b --porcelain | egrep -ox '## (.*)' | cut -d" " -f2 | awk -F'[.]{3}' '{print $1}')
 		set head (git log -1 --abbrev-commit --pretty=format:%h)
-		set gitstatus (git status -b --porcelain)
 		set outgoing (git log -1 --pretty=format:%h --abbrev-commit $branch --not --remotes)
 		set incoming (git log -1 --abbrev-commit --pretty=format:%h --remotes --not $branch)
 		# print the branch name
@@ -14,13 +13,13 @@ function git_prompt
 		# print the indicator
 		echo -n (set_color --bold red)
 		# red question mark: unversioned files present
-		if test (echo $gitstatus | egrep -ox '\?\? (.*)' | head -n1)
+		if test (git status -b --porcelain | egrep -ox '\?\? (.*)' | head -n1)
 			echo -n (set_color --bold red)"?"
 		# red exclamation point: uncommited, unadded changes
-		else if test (echo $gitstatus | egrep -ox ' [UMADRC] (.*)' | head -n1)
+		else if test (git status -b --porcelain | egrep -ox ' [UMADRC] (.*)' | head -n1)
 			echo -n (set_color --bold red)"!"
 		# green exclamation point: uncommitted, added changes
-		else if test (echo $gitstatus | egrep -ox '[UMADRC]  (.*)' | head -n1)
+		else if test (git status -b --porcelain | egrep -ox '[UMADRC]  (.*)' | head -n1)
 			echo -n (set_color --bold green)"!"
 		# red minus: outgoing AND incoming commits
 		else if test (echo $outgoing)
