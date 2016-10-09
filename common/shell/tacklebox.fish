4c5a896a181d854187ba18722a536025e411b53d
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2016-10-09 19:36:31 +0200

    Updated tacklebox, removed oh-my-fish

--- common/shell/tacklebox.fish
+++ common/shell/tacklebox.fish
@@ -1,7 +1,5 @@
 # Tacklebox repositories
-set tacklebox_path ~/.tackle/bpinto/oh-my-fish \
-                   ~/.tackle/urdh/oh-my-fish \
-                   ~/.tackle/justinmayer/tackle \
+set tacklebox_path ~/.tackle/justinmayer/tackle \
                    ~/.tackle/urdh/tackle
 
 # Initial setup, including theme
@@ -13,11 +11,6 @@ set tacklebox_theme urdh
 set tacklebox_modules $tacklebox_modules
 set tacklebox_plugins $tacklebox_plugins \
                       up pip extract grc
-
-# Things from urdh/oh-my-fish and bpinto/oh-my-fish
-set tacklebox_modules $tacklebox_modules
-set tacklebox_plugins $tacklebox_plugins \
-                      bundler localhost 
 
 # Finally, load Tacklebox itself
 source ~/.tacklebox/tacklebox.fish
