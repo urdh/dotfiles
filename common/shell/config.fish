d60261c71f4f1005e09eb89394ee0230a2a4bbd5
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2019-04-24 22:20:35 +0200

    Remove tacklebox configuration

--- common/shell/config.fish
+++ common/shell/config.fish
@@ -1,6 +1,5 @@
 #!/usr/local/bin/fish
 if status --is-login
     source ~/.config/fish/profile.fish
-    source ~/.config/fish/tacklebox.fish
     source ~/.iterm2_shell_integration.fish ^/dev/null
 end
